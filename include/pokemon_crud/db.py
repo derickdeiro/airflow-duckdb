7a036ae5b37fb12823aee446d5eff3c55ec3f991
ddeiro <ddeiro@bdsdatasolution.com.br>
2024-08-14 20:58:56 -0300

    fix: Consertando a forma de realizar a conexão com o DB pelo Airflow Admin Connections

--- include/pokemon_crud/db.py
+++ include/pokemon_crud/db.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-RENDER_POSTGRES_CONN = PostgresOperator(postgres_conn_id='render_postgres_conn')
-engine = create_engine(RENDER_POSTGRES_CONN)
+render_postgres_conn = PostgresHook(postgres_conn_id='render_postgresdb_conn')
+
+postgresurl_conn = render_postgres_conn.get_uri()
+
+engine = create_engine(postgresurl_conn)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
